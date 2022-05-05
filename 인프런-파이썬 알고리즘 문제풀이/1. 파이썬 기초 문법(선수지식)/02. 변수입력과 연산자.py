227914ded2660b7bdd91f45b15e6988c5787fa78
zzerowin <alaykr123@gmail.com>
2022-05-06 00:53:32 +0900

    변수입력과 연산자

--- 인프런-파이썬 알고리즘 문제풀이/1. 파이썬 기초 문법(선수지식)/02. 변수입력과 연산자.py
+++ 인프런-파이썬 알고리즘 문제풀이/1. 파이썬 기초 문법(선수지식)/02. 변수입력과 연산자.py
@@ -1,3 +1,29 @@
 """
 변수입력과 연산자
 """
+
+a = input("숫자를 입력하세요: ")
+print(a)
+
+
+a, b = input("숫자를 입력하세요: ").split()
+print(a + b)
+
+a, b = input("숫자를 입력하세요: ").split()
+a = int(a)
+b = int(b)
+print(a + b)
+
+a, b = map(int, input("숫자를 입력하세요: ").split())
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+a = 4.3
+b = 5
+c = a + b
+print(c)
