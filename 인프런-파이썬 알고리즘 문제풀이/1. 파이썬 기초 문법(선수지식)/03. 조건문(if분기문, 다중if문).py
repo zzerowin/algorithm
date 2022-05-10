fd3812b07913af9f55143bd02347d374cf37a21d
zzerowin <alaykr123@gmail.com>
2022-05-10 19:49:24 +0900

    조건문 if(분기, 중첩)

---
+++ 인프런-파이썬 알고리즘 문제풀이/1. 파이썬 기초 문법(선수지식)/03. 조건문(if분기문, 다중if문).py
@@ -0,0 +1,40 @@
+"""
+조건문 if(분기, 중첩)
+"""
+
+x = 9
+if x >= 10:
+    if x % 2 == 1:
+        print("10이상의 홀수")
+
+x = 10
+if x > 0:
+    if x < 10:
+        print("10보다 작은 자연수")
+
+x = 7
+if x > 0 and x < 10:
+    print("10보다 작은 자연수")
+
+
+x = 7
+if 0 < x < 10:
+    print("10보다 작은 자연수")
+
+x = -3
+if x > 0:
+    print("양수")
+else:
+    print("음수")
+
+x = 93
+if x >= 90:
+    print('A 학첨')
+elif x >= 80:
+    print('B 학점')
+elif x >= 70:
+    print("C 학점")
+elif x >= 60:
+    print("D 학점")
+else:
+    print("F 학점")
