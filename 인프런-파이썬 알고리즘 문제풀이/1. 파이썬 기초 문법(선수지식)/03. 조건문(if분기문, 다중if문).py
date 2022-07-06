7558e98bcf8b2e7f0f4955678903c842d5258b21
zzerowin <alaykr123@gmail.com>
2022-07-06 10:37:14 +0900

    반복문(for, while, break, continue)

--- 인프런-파이썬 알고리즘 문제풀이/1. 파이썬 기초 문법(선수지식)/03. 조건문(if분기문, 다중if문).py
+++ 인프런-파이썬 알고리즘 문제풀이/1. 파이썬 기초 문법(선수지식)/03. 조건문(if분기문, 다중if문).py
@@ -2,10 +2,11 @@
 조건문 if(분기, 중첩)
 """
 
-x = 9
+x = 15
 if x >= 10:
     if x % 2 == 1:
         print("10이상의 홀수")
+
 
 x = 10
 if x > 0:
@@ -16,10 +17,10 @@ x = 7
 if x > 0 and x < 10:
     print("10보다 작은 자연수")
 
-
 x = 7
 if 0 < x < 10:
     print("10보다 작은 자연수")
+
 
 x = -3
 if x > 0:
@@ -29,12 +30,12 @@ else:
 
 x = 93
 if x >= 90:
-    print('A 학첨')
+    print('A')
 elif x >= 80:
-    print('B 학점')
+    print('B')
 elif x >= 70:
-    print("C 학점")
+    print("C")
 elif x >= 60:
-    print("D 학점")
+    print("D")
 else:
-    print("F 학점")
+    print("F")
---
+++ 인프런-파이썬 알고리즘 문제풀이/1. 파이썬 기초 문법(선수지식)/04. 반복문(for, while, break, continue).py
@@ -0,0 +1,42 @@
+"""
+반복문 (for, while)
+
+a = range(1, 11)
+print(list(a))
+
+for i in range(1, 11):
+    print(i)
+
+for i in range(10, 0, -2):
+    print(i)
+
+
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+
+i = 10
+while i >= 1:
+    print(i)
+    i -= 1
+
+i = 1
+while True:
+    print(i)
+    if i == 10:
+        break
+    i += 1
+
+for i in range(1, 11):
+    if i % 2 == 0:
+        continue
+    print(i)
+"""
+
+for i in range(1, 11):
+    print(i)
+    if i > 15:
+        break
+else:
+    print(11)
