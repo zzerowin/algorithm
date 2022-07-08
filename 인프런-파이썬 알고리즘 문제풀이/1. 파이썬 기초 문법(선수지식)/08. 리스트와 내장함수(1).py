802cd66c9db0c80e8735297346de6d1a77af683e
zzerowin <alaykr123@gmail.com>
2022-07-08 09:42:09 +0900

    리스트와 내장함수(1)

---
+++ 인프런-파이썬 알고리즘 문제풀이/1. 파이썬 기초 문법(선수지식)/08. 리스트와 내장함수(1).py
@@ -0,0 +1,50 @@
+"""
+리스트와 내장함수(1)
+"""
+import random as r
+b = list()
+# print(b)
+
+a = [1, 2, 3, 4, 5]
+# print(a)
+# print(a[0])
+
+b = list(range(1, 11))
+# print(b)
+
+c = a + b
+# print(c)
+
+print(a)
+a.append(6)
+print(a)
+
+a.insert(3, 7)
+print(a)
+
+a.pop()
+print(a)
+a.pop(3)
+print(a)
+
+a.remove(4)
+print(a)
+
+print(a.index(5))
+
+a = list(range(1, 11))
+print(sum(a))
+print(max(a))
+print(min(a))
+print(min(7, 3, 5))
+print(a)
+
+r.shuffle(a)
+print(a)
+a.sort(reverse=True)
+print(a)
+a.sort()
+print(a)
+
+a.clear()
+print(a)
