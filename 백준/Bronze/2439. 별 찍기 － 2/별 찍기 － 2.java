d3b9d60576035e7314f3717e4a9ef031f8e92ae7
zzerowin <99893696+zzerowin@users.noreply.github.com>
2022-07-30 22:41:58 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 256 ms, Memory: 15076 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = n - 1; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
