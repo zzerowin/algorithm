12d5753070d9ea73e43bd2dd0669970fe7bb4555
zzerowin <99893696+zzerowin@users.noreply.github.com>
2022-09-07 23:23:20 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -0,0 +1,10 @@
+function solution(num) {
+    var answer = '';
+    if (num % 2 === 0) {
+        return answer = 'Even';
+    } else {
+        return answer = 'Odd';
+    }
+    
+    return answer;
+}
