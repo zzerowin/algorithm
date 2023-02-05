7e1953a987f70f221c11052929545f39284364b1
zzerowin <99893696+zzerowin@users.noreply.github.com>
2023-02-05 17:17:39 +0900

    [Bronze III] Title: 최소, 최대, Time: 500 ms, Memory: 88700 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.js
@@ -0,0 +1,19 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+
+let count = Number(input[0]);
+let numbers = input[1].split(' ').map(x => Number(x));
+
+let max = numbers[0];
+let min = numbers[0];
+
+for (let i = 0; i < count; i++) {
+    if (max < numbers[i]) {
+        max = numbers[i];
+    }
+
+    if (min > numbers[i]) {
+        min = numbers[i];
+    }
+}
+
+console.log(`${min} ${max}`);
