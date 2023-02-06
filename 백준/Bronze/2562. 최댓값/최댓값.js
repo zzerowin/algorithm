909030384ce7e5a26c7446527aadf9e03b93954a
zzerowin <99893696+zzerowin@users.noreply.github.com>
2023-02-06 15:00:20 +0900

    [Bronze III] Title: 최댓값, Time: 120 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.js
@@ -0,0 +1,14 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().split('\n').map(x => Number(x));
+
+let max = input[0];
+let maxIdx = 0;
+
+for (let i = 0; i < 9; i++) {
+    if (max < input[i]) {
+        max = input[i];
+        maxIdx = i;
+    }
+}
+
+console.log(max);
+console.log(maxIdx + 1);
