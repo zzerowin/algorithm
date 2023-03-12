2578c818c5c6494291e866b2a89795747a23a7c8
zzerowin <99893696+zzerowin@users.noreply.github.com>
2023-03-12 15:37:11 +0900

    [level 0] Title: 배열의 평균값, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
@@ -0,0 +1,10 @@
+const solution = (numbers) => {
+    let answer = 0;
+    let sum = 0;
+    
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i] 
+    }
+    
+    return answer = sum / numbers.length;
+}
