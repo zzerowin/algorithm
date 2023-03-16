f512d621c4f78e8eac276bd1000a9bf3431c2554
zzerowin <99893696+zzerowin@users.noreply.github.com>
2023-03-16 23:35:58 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -0,0 +1,16 @@
+const solution = (emergency) => {
+    const copiedEmergency = [...emergency].sort((a, b) => b - a);
+    let rankArr = new Array(emergency.length).fill(0);
+    
+    let count = 1;
+    
+    for (let i = 0; i < emergency.length; i++) {
+        let index = emergency.indexOf(copiedEmergency[i])
+        
+        rankArr.splice(index, 1, count);
+        
+        count++;
+    }
+    
+    return rankArr;
+}
