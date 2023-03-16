f5f874e48de8e5dec3327b2658b89fb3fc9e2aac
zzerowin <99893696+zzerowin@users.noreply.github.com>
2023-03-16 23:29:28 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -0,0 +1,18 @@
+const solution = (age) => {
+    let alpha = {
+        0: 'a',
+        1 : "b",
+        2 : "c",
+        3 : "d",
+        4 : "e",
+        5 : "f",
+        6 : "g",
+        7 : "h",
+        8 : "i",
+        9 : "j"
+    }
+    
+    let newAge = age + '';
+    
+    return newAge.split('').map((it) => alpha[it]).join('');
+}
