4ec23b188f7c0af07b17ff9117ec814f286d35eb
zzerowin <99893696+zzerowin@users.noreply.github.com>
2023-03-21 23:14:00 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -0,0 +1,9 @@
+const solution = (num_list, n) => {
+    let result = [];
+    
+    for (let i = 0; i < num_list.length / n; i++) {
+        result = [...result, num_list.slice(i * n, i * n + n)];
+    }
+    
+    return result;
+}
