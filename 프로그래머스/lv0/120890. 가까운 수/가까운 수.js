b68d780d7495a4b339a07046723d113df8b81b49
zzerowin <99893696+zzerowin@users.noreply.github.com>
2023-03-25 18:42:25 +0900

    [level 0] Title: 가까운 수, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -0,0 +1,4 @@
+const solution = (array, n) => {
+    const minDiff = Math.min(...array.map(a => Math.abs(a - n)));
+    return array.sort((a, b) => a - b).find(a => Math.abs(a - n) === minDiff);
+}
