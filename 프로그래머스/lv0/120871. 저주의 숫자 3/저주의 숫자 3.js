9604ce9facdefc769dcf59b7f39337b0daf1dfeb
zzerowin <99893696+zzerowin@users.noreply.github.com>
2023-04-02 22:50:01 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -0,0 +1,11 @@
+const solution = (n) => {
+    let num = 0;
+    let count = 0;
+    
+    while (count < n) {
+        num += 1;
+        if (!('' + num).includes('3') && num % 3 !== 0) count += 1;
+    }
+    
+    return num;
+}
