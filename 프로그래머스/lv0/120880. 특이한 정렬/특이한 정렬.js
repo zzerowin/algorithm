88f9b6c3cde5e13b25b0fd5b5eea195abcf5254f
zzerowin <99893696+zzerowin@users.noreply.github.com>
2023-04-03 19:12:52 +0900

    [level 0] Title: 특이한 정렬, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
@@ -0,0 +1,7 @@
+const solution = (numlist, n) => {
+    return numlist.sort((a, b) => {
+        const [aDiff, bDiff] = [Math.abs(a - n), Math.abs(b - n)];
+        if (aDiff === bDiff) return b - a;
+        return aDiff - bDiff;
+    })
+}
