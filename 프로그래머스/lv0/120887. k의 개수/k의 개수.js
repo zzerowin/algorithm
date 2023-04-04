d470865e4bc0253d79139b9d18f5f3281fea6351
zzerowin <99893696+zzerowin@users.noreply.github.com>
2023-04-04 20:10:22 +0900

    [level 0] Title: k의 개수, Time: 10.68 ms, Memory: 38.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.js
@@ -0,0 +1,8 @@
+const solution = (i, j, k) => {
+    let count = 0;
+    for (let num = i; num <= j; num++) {
+        count += [...('' + num)].filter(n => +n === k).length;
+    }
+    
+    return count;
+}
