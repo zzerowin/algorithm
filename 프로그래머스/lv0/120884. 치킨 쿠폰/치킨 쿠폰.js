0e565e5521c746b6b9b186151f1e37c343efc924
zzerowin <99893696+zzerowin@users.noreply.github.com>
2023-04-04 20:02:34 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.04 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -0,0 +1,9 @@
+const solution = (chicken) => {
+    const order = coupons => {
+        if (coupons < 10) return 0;
+        const service = Math.floor(coupons / 10);
+        return service + order(service + (coupons % 10));
+    }
+    
+    return order(chicken);
+}
