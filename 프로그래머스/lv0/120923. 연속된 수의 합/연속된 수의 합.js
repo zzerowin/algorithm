8469e584108cce648d49803143d09ba11c860785
zzerowin <99893696+zzerowin@users.noreply.github.com>
2023-04-05 14:43:59 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.js
@@ -0,0 +1,5 @@
+const solution = (num, total) => {
+    const numArr = Array.from({length: num}, (_, i) => i);
+    const sum = numArr.reduce((a, c) => a + c);
+    return numArr.map(n => n - (sum - total) / num);
+}
